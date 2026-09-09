d430f83bf92097656779c93712febd98f782d4de
Raygenly Cas-ing <raygenlycas@gmail.com>
2026-09-10 01:02:09 +0800

    Phase 3: Implement answer checking and score state

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -22,6 +22,12 @@ fun GameScreen() {
         mutableStateOf("")
     }
 
+    val correctAnswer = "CAT"
+
+    var score by remember {
+        mutableStateOf(0)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -53,13 +59,17 @@ fun GameScreen() {
         )
 
         Button(
-            onClick = { }
+            onClick = {
+                if (userAnswer == correctAnswer) {
+                    score++
+                }
+            }
         ) {
             Text("SUBMIT")
         }
 
         Text(
-            text = "Score: 0"
+            text = "Score: $score"
         )
     }
 }
